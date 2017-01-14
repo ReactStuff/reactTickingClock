7edf6aa41abdfa25d7bcf41c66819e8e81bb0ac6
Ninan <jninan@thoughtwork.com>
2017-01-14 20:55:47 +0530

    #4 [Ninan/ReactDocs] Implemented state and lifecycle event for Clock

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
-class Clock extends React.Component{
-    render(){
+class Clock extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
         return (
             <div className="Clock">
-                Its {this.props.date}
+                Its {this.state.date.toLocaleTimeString()}
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-function tick() {
-    ReactDOM.render(
-        <App />,
-        document.getElementById('root')
-    );
-}
 
-setInterval(tick, 1000);
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
+
